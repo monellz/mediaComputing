f5e6dadb0259e5d817c69601056c2711aced0a93
monell <monell.zhong@gmail.com>
2019-11-10 10:58:21 +0800

    nearly done

--- coordinate_based_image_cloning/src/cloning.rs
+++ coordinate_based_image_cloning/src/cloning.rs
@@ -3,8 +3,9 @@ use crate::*;
 
 pub fn process(mut img: CloningImage) -> CloningImage {
     let dis2 = |a: (usize, usize), b: (usize, usize)| {
-        let t = a.0 as f64 - b.0 as f64;
-        t * t
+        let x = a.0 as f64 - b.0 as f64;
+        let y = a.1 as f64 - b.1 as f64;
+        x * x + y * y
     };
     let half_tan = |a: (usize, usize), b: (usize, usize), c: (usize, usize)| {
         // <a, b, c
@@ -17,20 +18,26 @@ pub fn process(mut img: CloningImage) -> CloningImage {
         ((1.0 - cos) / (1.0 + cos)).sqrt()
     };
 
+    let normalize = |x: f64| {
+        if x > 1.0 { return 1.0f64; }
+        else if x < 0.0 { return 0.0f64; }
+        x
+    };
+
 
     (0..img.fg_mask_idx.len()).for_each(|i| {
-        if i % 100 == 0 {
+        if i % 500 == 0 {
             info!("pixel {}/{}", i, img.fg_mask_idx.len());
         }
         let local_i = img.fg_mask_idx[i];
         //compute the mean-value coordinates
-        let mut mvc = Vec::<f64>::with_capacity(img.fg_bound_idx.len());
+        let mut mvc = Vec::<f64>::with_capacity(img.fg_bound.len());
         let mut sum: f64 = 0.0;
-        img.fg_bound_idx.iter().enumerate().for_each(|(b, &cur)| {
-            let prev = if b == 0 { img.fg_bound_idx[img.fg_bound_idx.len() - 1] } else { img.fg_bound_idx[b - 1] };
-            let next = if b == img.fg_bound_idx.len() - 1 { img.fg_bound_idx[0] } else { img.fg_bound_idx[b + 1] };
+        img.fg_bound.iter().enumerate().for_each(|(b, &cur)| {
+            let prev = if b == 0 { img.fg_bound[img.fg_bound.len() - 1] } else { img.fg_bound[b - 1] };
+            let next = if b == img.fg_bound.len() - 1 { img.fg_bound[0] } else { img.fg_bound[b + 1] };
 
-            let w = (half_tan(prev, local_i, cur) + half_tan(cur, local_i, next)) / dis2(cur, local_i).sqrt();
+            let w = (half_tan((prev.0, prev.1), local_i, (cur.0, cur.1)) + half_tan((cur.0, cur.1), local_i, (next.0, next.1))) / dis2((cur.0, cur.1), local_i).sqrt();
 
             sum += w;
             mvc.push(w);
@@ -43,13 +50,14 @@ pub fn process(mut img: CloningImage) -> CloningImage {
 
         //evaluate the mean-value interpolant 
         let mut r: [f64; 3] = [0.0, 0.0, 0.0];
-        r[0] = mvc.iter().enumerate().fold(0.0, |acc, (i, &v)| acc + v * img.bound_diff[0][i]);
-        r[1] = mvc.iter().enumerate().fold(0.0, |acc, (i, &v)| acc + v * img.bound_diff[1][i]);
-        r[2] = mvc.iter().enumerate().fold(0.0, |acc, (i, &v)| acc + v * img.bound_diff[2][i]);
+        r[0] = mvc.iter().enumerate().fold(0.0, |acc, (i, &v)| acc + v * img.fg_bound[i].2);
+        r[1] = mvc.iter().enumerate().fold(0.0, |acc, (i, &v)| acc + v * img.fg_bound[i].3);
+        r[2] = mvc.iter().enumerate().fold(0.0, |acc, (i, &v)| acc + v * img.fg_bound[i].4);
         let global_i =  img.bg_mask_idx[i];
-        img.bg_mat[(0, global_i.0, global_i.1)] = r[0] + img.fg_mat[(0, local_i.0, local_i.1)]; 
-        img.bg_mat[(1, global_i.0, global_i.1)] = r[1] + img.fg_mat[(1, local_i.0, local_i.1)]; 
-        img.bg_mat[(2, global_i.0, global_i.1)] = r[2] + img.fg_mat[(2, local_i.0, local_i.1)]; 
+        img.bg_mat[(0, global_i.0, global_i.1)] = normalize(r[0] + img.fg_mat[(0, local_i.0, local_i.1)]); 
+        img.bg_mat[(1, global_i.0, global_i.1)] = normalize(r[1] + img.fg_mat[(1, local_i.0, local_i.1)]); 
+        img.bg_mat[(2, global_i.0, global_i.1)] = normalize(r[2] + img.fg_mat[(2, local_i.0, local_i.1)]); 
+
     });
 
     img
@@ -61,10 +69,8 @@ pub struct CloningImage {
     fg_mat:         RgbMatrix,
     fg_mask_idx:    Vec<(usize, usize)>,
     bg_mask_idx:    Vec<(usize, usize)>,
-
-
-    fg_bound_idx:   Vec<(usize, usize)>,
-    bound_diff:     [Vec<f64>; 3] 
+    //(x, y, r, b, b)
+    fg_bound:       Vec<(usize, usize, f64, f64, f64)>
 }
 
 impl CloningImage {
@@ -74,14 +80,12 @@ impl CloningImage {
         let mut fg_mask_idx: Vec<(usize, usize)> = vec![];
         let mut bg_mask_idx: Vec<(usize, usize)> = vec![];
 
-        let mut fg_bound_idx: Vec<(usize, usize)> = vec![];
-        let mut bound_diff_r: Vec<f64> = vec![];
-        let mut bound_diff_g: Vec<f64> = vec![];
-        let mut bound_diff_b: Vec<f64> = vec![];
-
+        let mut fg_bound: Vec<(usize, usize, f64, f64, f64)> = vec![];
 
         let dx = [0, 0, 1, -1];
         let dy = [1, -1, 0, 0];
+
+        let mut center = (0, 0);
 
         for i in 0..fg_mask_mat.elem.len() {
             let local = (i / fg_mask_mat.ncol, i % fg_mask_mat.ncol);
@@ -99,32 +103,42 @@ impl CloningImage {
                             let local_nbh = (local_nbh.0 as usize, local_nbh.1 as usize);
                             if let Some(_) = fg_mask_mat[local_nbh] {
                                 //local is in boundary
-                                fg_bound_idx.push(local);
+                                let global = (offset.0 + local.0, offset.1 + local.1);
+                                let r = bg_mat[(0, global.0, global.1)] - fg_mat[(0, local.0, local.1)];
+                                let g = bg_mat[(1, global.0, global.1)] - fg_mat[(1, local.0, local.1)];
+                                let b = bg_mat[(2, global.0, global.1)] - fg_mat[(2, local.0, local.1)];
 
-                                let global = (offset.0 + local.0 as usize, offset.1 + local.1 as usize);
+                                fg_bound.push((local.0, local.1, r, g, b));
 
-                                bound_diff_r.push(bg_mat[(0, global.0, global.1)] - fg_mat[(0, local.0, local.1)]);
-                                bound_diff_g.push(bg_mat[(1, global.0, global.1)] - fg_mat[(1, local.0, local.1)]);
-                                bound_diff_b.push(bg_mat[(2, global.0, global.1)] - fg_mat[(2, local.0, local.1)]);
-
+                                center.0 += local.0;
+                                center.1 += local.1;
                                 break;
                             }
                         }
                     }
                 }
             };
- 
         }
 
+        let center = (center.0 as f64 / fg_bound.len() as f64, center.1 as f64 / fg_bound.len() as f64);
+        fg_bound.sort_by(|a, b| {
+            if a.0 == 0 && b.0 == 0 { return (a.1).cmp(&b.1).reverse(); }
+
+            let det = (a.0 as f64 - center.0) * (b.1 as f64 - center.1) - (b.0 as f64 - center.0) * (a.1 as f64 - center.1);
+            if det < 0.0 { return std::cmp::Ordering::Less; }
+            else if det > 0.0 { return std::cmp::Ordering::Greater; }
+
+            let a_len2 = (a.0 as f64 - center.0) * (a.0 as f64 - center.0) + (a.1 as f64 - center.1) * (a.1 as f64 - center.1);
+            let b_len2 = (b.0 as f64 - center.0) * (b.0 as f64 - center.0) + (b.1 as f64 - center.1) * (b.1 as f64 - center.1);
+            a_len2.partial_cmp(&b_len2).unwrap().reverse()
+        });
 
         CloningImage {
             bg_mat,
             fg_mat,
             fg_mask_idx,
             bg_mask_idx,
-
-            fg_bound_idx,
-            bound_diff: [bound_diff_r, bound_diff_g, bound_diff_b]
+            fg_bound
         }
     }
 }
